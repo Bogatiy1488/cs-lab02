4d9d78a0c929a6d5c26e6beada4c1cadfcba6d06
u211-17 <u211-17@public.mpei.local>
2019-03-04 12:00:55 +0300

    Bob added 1 more fckn mult

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
-    cin >> a >> b
+    cin >> a >> b;
 
-    cout << "A + B = " << a + b << '\n'
+    cout << "A + B = " << a + b << '\n';
     cout  << "A - B = " << a - b << '\n';
 
 
     cout<<"A x B = " << a * b << '\n';
+     cout<<"A x B = " << a * b << '\n';
     return 0;
 }
